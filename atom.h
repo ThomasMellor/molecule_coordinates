52a978d615a4eb47198238a9980f44ffed5718eb
ThomasMellor <tom.mel@hotmail.co.uk>
2018-02-11 19:32:19 +0000

    atom

--- atom.h
+++ atom.h
@@ -3,22 +3,24 @@
 
 #include <string>
 #include <vector>
-#include <memory>
 
 class atom {
 	private:
 		const unsigned int number;
 		const std::string name;
 		std::vector<double> cart_coords = {0,0,0};
-		std::shared_ptr<atom> bond_length_atom;
-		std::vector<std::shared_ptr<atom>> angle_atoms;
-		std::vector<std::shared_ptr<atom>> dihedral_angle_atoms;
+		const atom *bond_length_atom;
+		std::vector<const atom*> angle_atoms;
+		std::vector<const atom*> dihedral_angle_atoms;
 	public:
 		atom(unsigned int num, std::string name);	
 		std::string get_name() const; 
 		int get_number() const;
 		std::vector<double> get_cart_coords() const;
 		void set_cart_coords(double x, double y, double z); 
+		void set_bond_length_atom(atom const& connected_to);
+		void set_angle_atoms(atom const& atom_1, atom const& atom_2);
+		void set_dihedral_angle_atoms(atom const& atom_1, atom const& atom_2, atom const& atom_3);
 };
 
 #endif
