627390d240bc0e5ca5e7df753a4168abe5d87929
ThomasMellor <tom.mel@hotmail.co.uk>
2018-02-11 19:29:03 +0000

    atom

--- atom.cc
+++ atom.cc
@@ -18,3 +18,15 @@ void atom::set_cart_coords(double x, double y, double z) {
 	cart_coords = {x, y, z};
 	return;
 };
+
+void atom::set_bond_length_atom(atom const& connected_to) {
+	bond_length_atom = &connected_to;
+};
+
+void atom::set_angle_atoms(atom const& atom_1, atom const& atom_2) {
+	angle_atoms = {&atom_1, &atom_2};
+};
+
+void atom::set_dihedral_angle_atoms(atom const& atom_1, atom const& atom_2, atom const& atom_3) {
+	dihedral_angle_atoms = {&atom_1, &atom_2, &atom_3};
+};
