fbd8a3be6ef9aa7681dd75a2d04495266c3161c6
ThomasMellor <tom.mel@hotmail.co.uk>
2018-03-04 20:19:10 +0000

    conversion complete

--- test.cc
+++ test.cc
@@ -4,29 +4,25 @@
 #include "eigen-eigen-5a0156e40feb/Eigen/Dense"
 
 int main() {
-	molecule mol("./test_2.txt", "CH4");
-	mol.set_molecule_coord("./test_coords_2.txt");
+	molecule mol("./test.txt", "CH4");
+	mol.set_molecule_coord("./test_coords_1.txt");
 	mol.print_coordinates();
-	std::vector<std::vector<double>> test_coord= {{},{1},{1,2}};
+	std::vector<std::vector<double>> test_coord= {{},{1},{1,1}, {1,2,0.1},{1.4,2,3},{1,2,0.1},{1,2,2},{2,2,-2}};
 	Eigen::MatrixXd mat = mol.empty_matrix();
 	Eigen::VectorXd vec = mol.empty_vector();
 	Eigen::VectorXd x = mol.empty_vector();
 	mol.derivative_vector(vec, test_coord);
-	std::cout << vec << std::endl;
 	mol.derivative_matrix(mat);
-	std::cout << mat << std::endl;	
-	std::cout << mol.bond_length_derivative(2,2, "x") << std::endl;
-	std::cout << mol.angle_derivative(3,2, "x") << std::endl;
-	std::cout << mol.bond_length_derivative(3,2,"x") << std::endl;
 	int counter = 0;
-//while((mol.coord_difference(test_coord) > 0.001) || (counter < 10000)) {
-//		mol.derivative_matrix(mat);
-		//mol.derivative_vector(vec, test_coord);
+while((mol.coord_difference(test_coord) > 0.00000001) || (counter < 5)) {
+		mol.derivative_matrix(mat);
+		mol.derivative_vector(vec, test_coord);
 		x = mat.colPivHouseholderQr().solve(vec);
-		std::cout << x << std::endl;
+		x =-x;
+		counter++;
 		mol.update_molecule_coord(x);	
-		//std::cout << counter << " " << mol.coord_difference(test_coord) << std::endl;
-		//mol.print_coordinates();
-	//};
-	mol.print_coordinates();
+		std::cout << counter << " " << mol.coord_difference(test_coord) << std::endl;
+		mol.print_coordinates();
+	};
+	mol.print_coordinates(); 
 };
